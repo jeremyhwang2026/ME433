94d0e544bafba52b4bf8d3247423d8d01dab3d5e
jeremyhwang2026 <138175213+jeremyhwang2026@users.noreply.github.com>
2026-05-14 16:14:30 -0500

    HW6

---
+++ HW6/dev_hid_composite/main.c
@@ -0,0 +1,469 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Ha Thach (tinyusb.org)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "bsp/board_api.h"
+#include "tusb.h"
+
+#include "usb_descriptors.h"
+
+//......
+
+#define I2C_PORT i2c0
+#define I2C_SDA 0
+#define I2C_SCL 1
+
+#include <math.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "hardware/adc.h"
+#include "hardware/gpio.h"
+
+#define BUTTON_PIN 20
+#define LED_PIN 17
+
+static bool remote_mode = false;
+
+#define MPU_ADDR 0x68
+#define ACCEL_XOUT_H 0x3B
+#define PWR_MGMT_1 0x6B
+#define GYRO_CONFIG 0x1B
+#define ACCEL_CONFIG 0x1C
+#define WHO_AM_I 0x75
+
+void mpu6050_init();
+void mpu6050_read_raw(int16_t *accelX, int16_t *accelY);
+void setPin(unsigned char address, unsigned char reg, unsigned char value);
+unsigned char readPin(unsigned char address, unsigned char reg);
+
+//.....
+
+//--------------------------------------------------------------------+
+// MACRO CONSTANT TYPEDEF PROTYPES
+//--------------------------------------------------------------------+
+
+/* Blink pattern
+ * - 250 ms  : device not mounted
+ * - 1000 ms : device mounted
+ * - 2500 ms : device is suspended
+ */
+enum
+{
+  BLINK_NOT_MOUNTED = 250,
+  BLINK_MOUNTED = 1000,
+  BLINK_SUSPENDED = 2500,
+};
+
+static uint32_t blink_interval_ms = BLINK_NOT_MOUNTED;
+
+void led_blinking_task(void);
+void hid_task(void);
+
+/*------------- MAIN -------------*/
+//////////////
+////////////
+
+int main(void)
+{
+  board_init();
+  stdio_init_all();
+
+  i2c_init(I2C_PORT, 400 * 1000); // 400kHz
+  gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+  gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+  gpio_pull_up(0);
+  gpio_pull_up(1);
+  mpu6050_init();
+
+  gpio_init(LED_PIN);
+  gpio_set_dir(LED_PIN, GPIO_OUT);
+
+  // init device stack on configured roothub port
+  tud_init(BOARD_TUD_RHPORT);
+
+  gpio_init(BUTTON_PIN);
+  gpio_set_dir(BUTTON_PIN, GPIO_IN);
+  gpio_pull_up(BUTTON_PIN);
+
+  if (board_init_after_tusb)
+  {
+    board_init_after_tusb();
+  }
+
+  while (1)
+  {
+    tud_task(); // tinyusb device task
+    led_blinking_task();
+
+    hid_task();
+
+    if (!gpio_get(BUTTON_PIN))
+    {
+      remote_mode = !remote_mode;
+      gpio_put(LED_PIN, remote_mode);
+      sleep_ms(100); // Simple debounce
+    }
+  }
+}
+
+//--------------------------------------------------------------------+
+// Device callbacks
+//--------------------------------------------------------------------+
+
+// Invoked when device is mounted
+void tud_mount_cb(void)
+{
+  blink_interval_ms = BLINK_MOUNTED;
+}
+
+// Invoked when device is unmounted
+void tud_umount_cb(void)
+{
+  blink_interval_ms = BLINK_NOT_MOUNTED;
+}
+
+// Invoked when usb bus is suspended
+// remote_wakeup_en : if host allow us  to perform remote wakeup
+// Within 7ms, device must draw an average of current less than 2.5 mA from bus
+void tud_suspend_cb(bool remote_wakeup_en)
+{
+  (void)remote_wakeup_en;
+  blink_interval_ms = BLINK_SUSPENDED;
+}
+
+// Invoked when usb bus is resumed
+void tud_resume_cb(void)
+{
+  blink_interval_ms = tud_mounted() ? BLINK_MOUNTED : BLINK_NOT_MOUNTED;
+}
+
+//--------------------------------------------------------------------+
+// USB HID
+//--------------------------------------------------------------------+
+
+static void send_hid_report(uint8_t report_id, uint32_t btn)
+{
+  // skip if hid is not ready yet
+  if (!tud_hid_ready())
+    return;
+
+  switch (report_id)
+  {
+  case REPORT_ID_KEYBOARD:
+  {
+    // use to avoid send multiple consecutive zero report for keyboard
+    static bool has_keyboard_key = false;
+
+    if (btn)
+    {
+      uint8_t keycode[6] = {0};
+      keycode[0] = HID_KEY_A;
+
+      tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, keycode);
+      has_keyboard_key = true;
+    }
+    else
+    {
+      // send empty key report if previously has key pressed
+      if (has_keyboard_key)
+        tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, NULL);
+      has_keyboard_key = false;
+    }
+  }
+  break;
+
+  case REPORT_ID_MOUSE:
+  {
+    int8_t delta_x = 0;
+    int8_t delta_y = 0;
+
+    if (remote_mode)
+    {
+      // REMOTE MODE: Move in a circle
+      static float angle = 0.0f;
+      angle += 0.1f;
+      delta_x = (int8_t)(cos(angle) * 10);
+      delta_y = (int8_t)(sin(angle) * 10);
+    }
+    else
+    {
+
+      int16_t ax, ay;
+      mpu6050_read_raw(&ax, &ay);
+
+      float fx = ax * 0.000061f;
+      float fy = ay * 0.000061f;
+
+      // X-Axis Speed Levels
+      if (fx > 0.7f)
+        delta_x = 8; // Fast
+      else if (fx > 0.4f)
+        delta_x = 4; // Medium
+      else if (fx > 0.15f)
+        delta_x = 2; // Slow
+      else if (fx < -0.7f)
+        delta_x = -8;
+      else if (fx < -0.4f)
+        delta_x = -4;
+      else if (fx < -0.15f)
+        delta_x = -2;
+
+      // Y-Axis Speed Levels
+      if (fy > 0.7f)
+        delta_y = -8;
+      else if (fy > 0.4f)
+        delta_y = -4;
+      else if (fy > 0.15f)
+        delta_y = -2;
+      else if (fy < -0.7f)
+        delta_y = 8;
+      else if (fy < -0.4f)
+        delta_y = 4;
+      else if (fy < -0.15f)
+        delta_y = 2;
+    }
+
+    tud_hid_mouse_report(REPORT_ID_MOUSE, 0x00, delta_x, delta_y, 0, 0);
+  }
+  break;
+
+  case REPORT_ID_CONSUMER_CONTROL:
+  {
+    // use to avoid send multiple consecutive zero report
+    static bool has_consumer_key = false;
+
+    if (btn)
+    {
+      // volume down
+      uint16_t volume_down = HID_USAGE_CONSUMER_VOLUME_DECREMENT;
+      tud_hid_report(REPORT_ID_CONSUMER_CONTROL, &volume_down, 2);
+      has_consumer_key = true;
+    }
+    else
+    {
+      // send empty key report (release key) if previously has key pressed
+      uint16_t empty_key = 0;
+      if (has_consumer_key)
+        tud_hid_report(REPORT_ID_CONSUMER_CONTROL, &empty_key, 2);
+      has_consumer_key = false;
+    }
+  }
+  break;
+
+  case REPORT_ID_GAMEPAD:
+  {
+    // use to avoid send multiple consecutive zero report for keyboard
+    static bool has_gamepad_key = false;
+
+    hid_gamepad_report_t report =
+        {
+            .x = 0, .y = 0, .z = 0, .rz = 0, .rx = 0, .ry = 0, .hat = 0, .buttons = 0};
+
+    if (btn)
+    {
+      report.hat = GAMEPAD_HAT_UP;
+      report.buttons = GAMEPAD_BUTTON_A;
+      tud_hid_report(REPORT_ID_GAMEPAD, &report, sizeof(report));
+
+      has_gamepad_key = true;
+    }
+    else
+    {
+      report.hat = GAMEPAD_HAT_CENTERED;
+      report.buttons = 0;
+      if (has_gamepad_key)
+        tud_hid_report(REPORT_ID_GAMEPAD, &report, sizeof(report));
+      has_gamepad_key = false;
+    }
+  }
+  break;
+
+  default:
+    break;
+  }
+}
+
+// Every 10ms, we will sent 1 report for each HID profile (keyboard, mouse etc ..)
+// tud_hid_report_complete_cb() is used to send the next report after previous one is complete
+void hid_task(void)
+{
+  // Poll every 10ms
+  const uint32_t interval_ms = 10;
+  static uint32_t start_ms = 0;
+
+  if (board_millis() - start_ms < interval_ms)
+    return; // not enough time
+  start_ms += interval_ms;
+
+  uint32_t const btn = board_button_read();
+
+  // Remote wakeup
+  if (tud_suspended() && btn)
+  {
+    // Wake up host if we are in suspend mode
+    // and REMOTE_WAKEUP feature is enabled by host
+    tud_remote_wakeup();
+  }
+  else
+  {
+    // Send the 1st of report chain, the rest will be sent by tud_hid_report_complete_cb()
+    send_hid_report(REPORT_ID_MOUSE, btn);
+  }
+}
+
+// Invoked when sent REPORT successfully to host
+// Application can use this to send the next report
+// Note: For composite reports, report[0] is report ID
+void tud_hid_report_complete_cb(uint8_t instance, uint8_t const *report, uint16_t len)
+{
+  (void)instance;
+  (void)len;
+
+  uint8_t next_report_id = report[0] + 1u;
+
+  if (next_report_id < REPORT_ID_COUNT)
+  {
+    send_hid_report(next_report_id, board_button_read());
+  }
+}
+
+// Invoked when received GET_REPORT control request
+// Application must fill buffer report's content and return its length.
+// Return zero will cause the stack to STALL request
+uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t *buffer, uint16_t reqlen)
+{
+  // TODO not Implemented
+  (void)instance;
+  (void)report_id;
+  (void)report_type;
+  (void)buffer;
+  (void)reqlen;
+
+  return 0;
+}
+
+// Invoked when received SET_REPORT control request or
+// received data on OUT endpoint ( Report ID = 0, Type = 0 )
+void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const *buffer, uint16_t bufsize)
+{
+  (void)instance;
+
+  if (report_type == HID_REPORT_TYPE_OUTPUT)
+  {
+    // Set keyboard LED e.g Capslock, Numlock etc...
+    if (report_id == REPORT_ID_KEYBOARD)
+    {
+      // bufsize should be (at least) 1
+      if (bufsize < 1)
+        return;
+
+      uint8_t const kbd_leds = buffer[0];
+
+      if (kbd_leds & KEYBOARD_LED_CAPSLOCK)
+      {
+        // Capslock On: disable blink, turn led on
+        blink_interval_ms = 0;
+        board_led_write(true);
+      }
+      else
+      {
+        // Caplocks Off: back to normal blink
+        board_led_write(false);
+        blink_interval_ms = BLINK_MOUNTED;
+      }
+    }
+  }
+}
+
+//--------------------------------------------------------------------+
+// BLINKING TASK
+//--------------------------------------------------------------------+
+void led_blinking_task(void)
+{
+  static uint32_t start_ms = 0;
+  static bool led_state = false;
+
+  // blink is disabled
+  if (!blink_interval_ms)
+    return;
+
+  // Blink every interval ms
+  if (board_millis() - start_ms < blink_interval_ms)
+    return; // not enough time
+  start_ms += blink_interval_ms;
+
+  board_led_write(led_state);
+  led_state = 1 - led_state; // toggle
+}
+
+void mpu6050_init()
+{
+  uint8_t id = readPin(MPU_ADDR, WHO_AM_I);
+  if (id != 0x68 && id != 0x98)
+  {
+    while (1)
+    { // Error
+      gpio_put(LED_PIN, 1);
+      sleep_ms(100);
+      gpio_put(LED_PIN, 0);
+      sleep_ms(100);
+    }
+  }
+
+  setPin(MPU_ADDR, PWR_MGMT_1, 0x00);   // Wake up
+  setPin(MPU_ADDR, ACCEL_CONFIG, 0x00); // 2g sensitivity
+  setPin(MPU_ADDR, GYRO_CONFIG, 0x18);  // 2000 dps sensitivity
+}
+
+void mpu6050_read_raw(int16_t *accelX, int16_t *accelY)
+{
+  uint8_t buffer[14];
+  uint8_t reg = ACCEL_XOUT_H;
+
+  i2c_write_blocking(i2c_default, MPU_ADDR, &reg, 1, true);
+  i2c_read_blocking(i2c_default, MPU_ADDR, buffer, 14, false);
+
+  *accelX = (buffer[0] << 8) | buffer[1];
+  *accelY = (buffer[2] << 8) | buffer[3];
+}
+
+void setPin(unsigned char address, unsigned char reg, unsigned char value)
+{
+  uint8_t buf[2];
+  buf[0] = reg;
+  buf[1] = value;
+  i2c_write_blocking(I2C_PORT, address, buf, 2, false);
+}
+
+unsigned char readPin(unsigned char address, unsigned char reg)
+{
+  uint8_t value;
+  i2c_write_blocking(I2C_PORT, address, &reg, 1, true);
+  i2c_read_blocking(I2C_PORT, address, &value, 1, false);
+  return (unsigned char)value;
+}
